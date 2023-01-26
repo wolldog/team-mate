49a938624c1a41fc91531973dc81b8c83d98f94e
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-01-26 17:23:32 +1100

    Add index.js starting point

---
+++ index.js
@@ -0,0 +1,11 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+
+// TODO: Create a function to write HTML file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
