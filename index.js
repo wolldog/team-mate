9916774bcc09c3141d144dda2ba2a2364f2fb971
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-01-29 16:16:29 +1100

    Add questions employees class & array for answers

--- index.js
+++ index.js
@@ -2,11 +2,46 @@
 const inquirer = require("inquirer");
 const fs = require("fs")
 
+const answers = [];
+
+const employeeQuestions = (currentNewMember) => {
+
+    inquirer 
+        .prompt ([ 
+            {
+                type: 'input',
+                name: 'name',
+                message: `What is the ${currentNewMember}'s name?`,
+                
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: `What is the ${currentNewMember}'s ID?`,
+                
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: `What is the ${currentNewMember}'s email?`,
+                
+            },
+        
+    ])
+    .then((response) => {
+        answers.push(response);
+        console.log(answers)
+    });
+};
 // TODO: Create a function to write HTML file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    let currentNewMember = "team manager";
+    console.log("Please build your team")
+    employeeQuestions (currentNewMember);
+}
 
 // Function call to initialize app
 init();
